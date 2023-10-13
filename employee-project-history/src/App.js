9106a3c9dde51f70fcd6ebc9101fac590361ace5
rightdirection1 <lyubima.takova@gmail.com>
2023-10-13 16:13:33 +0300

    initial

---
+++ employee-project-history/src/App.js
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div>
+      <ChakraProvider>
+      <h1>Hi</h1>
+    </ChakraProvider>
+    </div>
+  );
+}
+
+export default App;
